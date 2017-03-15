8b338ed240a50f69c071c2668dfd98b413cb1679
Leo Forney <forneyleo@gmail.com>
2017-03-14 23:14:49 -0500

    Major changes just to get Pulse Alert to connect to the Polar H7

--- app/src/main/java/tk/teamprotien/pulsealert/MainActivity.java
+++ app/src/main/java/tk/teamprotien/pulsealert/MainActivity.java
@@ -1,32 +1,12 @@
-package org.marco45.polarheartmonitor;
+package tk.teamprotien.pulsealert;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Observable;
-import java.util.Observer;
-
-import com.androidplot.xy.LineAndPointFormatter;
-import com.androidplot.xy.PointLabelFormatter;
-import com.androidplot.xy.XYPlot;
-import com.androidplot.xy.SimpleXYSeries;
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.firebase.analytics.FirebaseAnalytics;
-
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.DialogInterface;
-import android.content.Intent;
-import android.graphics.Color;
-import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -38,29 +18,28 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.marco45.pulse.R;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Observable;
+import java.util.Observer;
 
 /**
  * This program connect to a bluetooth polar heart rate monitor and display data
  *
  * @author Marco
  */
-public class MainActivity extends Activity implements OnItemSelectedListener, Observer {
+public class MainActivity extends AppCompatActivity implements OnItemSelectedListener, Observer {
 
-    private FirebaseAnalytics mFirebaseAnalytics;
     private int MAX_SIZE = 60; //graph max size
     boolean searchBt = true;
     BluetoothAdapter mBluetoothAdapter;
     List<BluetoothDevice> pairedDevices = new ArrayList<>();
     boolean menuBool = false; //display or not the disconnect option
-    private XYPlot plot;
     boolean h7 = false; //Was the BTLE tested
     boolean normal = false; //Was the BT tested
     private Spinner spinner1;
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    private GoogleApiClient client;
 
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,15 +47,6 @@ public class MainActivity extends Activity implements OnItemSelectedListener, Ob
         setContentView(R.layout.activity_main);
         Log.i("Main Activity", "Starting Polar HR monitor main activity");
         DataHandler.getInstance().addObserver(this);
-        AdView mAdView = (AdView) findViewById(R.id.adView);
-
-        // Create an ad request. Check logcat output for the hashed device ID to
-        // get test ads on a physical device. e.g.
-        // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
-        AdRequest adRequest = new AdRequest.Builder().build();
-
-        // Start loading the ad in the background.
-        mAdView.loadAd(adRequest);
 
         //Verify if device is to old for BTLE
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR2) {
@@ -90,7 +60,7 @@ public class MainActivity extends Activity implements OnItemSelectedListener, Ob
         if (DataHandler.getInstance().newValue) {
             //Verify if bluetooth if activated, if not activate it
             mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-            if(mBluetoothAdapter != null){
+            if (mBluetoothAdapter != null) {
                 if (!mBluetoothAdapter.isEnabled()) {
                     new AlertDialog.Builder(this)
                             .setTitle(R.string.bluetooth)
@@ -117,65 +87,15 @@ public class MainActivity extends Activity implements OnItemSelectedListener, Ob
                 }
             }
 
-
-
-            // Create Graph
-            plot = (XYPlot) findViewById(R.id.dynamicPlot);
-            if (plot.getSeriesSet().size() == 0) {
-                Number[] series1Numbers = {};
-                DataHandler.getInstance().setSeries1(new SimpleXYSeries(Arrays.asList(series1Numbers), SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "Heart Rate"));
-            }
-            DataHandler.getInstance().setNewValue(false);
-
         } else {
             listBT();
-            plot = (XYPlot) findViewById(R.id.dynamicPlot);
 
         }
-        //LOAD Graph
-        LineAndPointFormatter series1Format = new LineAndPointFormatter(Color.rgb(0, 0, 255), Color.rgb(200, 200, 200), null, null);
-        series1Format.setPointLabelFormatter(new PointLabelFormatter());
-        plot.addSeries(DataHandler.getInstance().getSeries1(), series1Format);
-        plot.setTicksPerRangeLabel(3);
-        plot.getGraphWidget().setDomainLabelOrientation(-45);
-
-        //ANALYTIC
-        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
-        Bundle bundle = new Bundle();
-        bundle.putString(FirebaseAnalytics.Param.VALUE, "Main");
-        mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle);
-        //t = GoogleAnalytics.getInstance(this).newTracker("UA-51478243-1");
-        //#t.setScreenName("Polar main page");
-        //#t.send(new HitBuilders.AppViewBuilder().build());
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     protected void onDestroy() {
         super.onDestroy();
         DataHandler.getInstance().deleteObserver(this);
-    }
-
-    public void onStart() {
-        super.onStart();
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Main Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app URL is correct.
-                Uri.parse("android-app://org.marco45.polarheartmonitor/http/host/path")
-        );
-        AppIndex.AppIndexApi.start(client, viewAction);
     }
 
     /**
@@ -234,6 +154,10 @@ public class MainActivity extends Activity implements OnItemSelectedListener, Ob
             spinner1.setOnItemSelectedListener(this);
             spinner1.setAdapter(dataAdapter);
 
+            for (int i = 0; i < dataAdapter.getCount(); i++ ) {
+                if (dataAdapter.getItem(i).contains("Polar H7")) spinner1.setSelection(i);
+            }
+
             if (DataHandler.getInstance().getID() != 0 && DataHandler.getInstance().getID() < spinner1.getCount())
                 spinner1.setSelection(DataHandler.getInstance().getID());
         }
@@ -263,11 +187,6 @@ public class MainActivity extends Activity implements OnItemSelectedListener, Ob
                 normal = false;
             }
             return true;
-        } else if (id == R.id.about) { //about menu
-
-            Log.i("Main Activity", "opening about");
-            Intent intent = new Intent(this, AboutActivity.class);
-            startActivity(intent);
         }
         return super.onOptionsItemSelected(item);
     }
@@ -372,42 +291,7 @@ public class MainActivity extends Activity implements OnItemSelectedListener, Ob
                 //menuBool=true;
                 TextView rpm = (TextView) findViewById(R.id.rpm);
                 rpm.setText(DataHandler.getInstance().getLastValue());
-
-                if (DataHandler.getInstance().getLastIntValue() != 0) {
-                    DataHandler.getInstance().getSeries1().addLast(0, DataHandler.getInstance().getLastIntValue());
-                    if (DataHandler.getInstance().getSeries1().size() > MAX_SIZE)
-                        DataHandler.getInstance().getSeries1().removeFirst();//Prevent graph to overload data.
-                    plot.redraw();
-                }
-
-                TextView min = (TextView) findViewById(R.id.min);
-                min.setText(DataHandler.getInstance().getMin());
-
-                TextView avg = (TextView) findViewById(R.id.avg);
-                avg.setText(DataHandler.getInstance().getAvg());
-
-                TextView max = (TextView) findViewById(R.id.max);
-                max.setText(DataHandler.getInstance().getMax());
             }
         });
-    }
-
-    public void onStop() {
-        super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Main Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app URL is correct.
-                Uri.parse("android-app://org.marco45.polarheartmonitor/http/host/path")
-        );
-        AppIndex.AppIndexApi.end(client, viewAction);
-        client.disconnect();
     }
 }
